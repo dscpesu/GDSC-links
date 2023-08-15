13c15abbed2dc04fcef6fdf757fe2e55a8229e28
Adithya S K <adithyaskolavi@gmail.com>
2023-08-15 13:03:02 +0530

    added link data - @adithya-s-k

---
+++ data/links.js
@@ -0,0 +1,53 @@
+const links = [
+  {
+    id: 1,
+    name: 'Join the Adventure: Recruitments',
+    about:
+      'Elevate your skills by applying for our exclusive recruitment drive.',
+    url: 'https://forms.gle/DBpxkxZPwDvoUMg68',
+    img: require('../public/assets/gdsc.webp'),
+  },
+  {
+    id: 2,
+    name: 'Stay Informed: WhatsApp Group',
+    about: 'Receive industry updates and engage in community discussions.',
+    url: 'https://chat.whatsapp.com/IHvpmXAMptz7AiDoJM4UJN',
+    img: require('../public/assets/whatsapp.webp'),
+  },
+
+  {
+    id: 3,
+    name: 'Connect on Instagram',
+    about:
+      'Follow our Instagram for insights into our achievements and initiatives.',
+    url: 'https://www.instagram.com/gdscpesu.rr/',
+    img: require('../public/assets/instagram.webp'),
+  },
+  {
+    id: 4,
+    name: 'Professional Network: LinkedIn',
+    about:
+      'Stay updated on GDSC activities and expand your professional network.',
+    url: 'https://www.linkedin.com/company/dscpesu',
+    img: require('../public/assets/linkedin.webp'),
+  },
+  {
+    id: 5,
+    name: 'Explore Projects: GitHub',
+    about:
+      'Discover our meticulously crafted projects and contributions on GitHub.',
+    url: 'https://github.com/dscpesu',
+    img: require('../public/assets/github.webp'),
+  },
+
+  {
+    id: 6,
+    name: 'Journey into the Past: Previous Website',
+    about:
+      'Explore our historical achievements by visiting our previous website.',
+    url: 'https://dscpesu.github.io/DSC-PESU/',
+    img: require('../public/assets/website.webp'),
+  },
+];
+
+export default links;
